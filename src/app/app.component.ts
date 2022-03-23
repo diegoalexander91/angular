fea556dd6dccb217d112d2653ebf1472f642c2eb
Diego Espitia <despitia@uniminuto.edu>
2022-03-23 11:21:30 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  //template: '<h1>Hola mundo,2</h1>',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'bases';
+  variable = 'Hola variable';
+  numero: number = 10;
+  base: number = 5;
+
+  calcular(num: number){
+    this.numero += num;
+  }
+
 }
